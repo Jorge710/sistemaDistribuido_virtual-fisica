4296f2ad79d08f2618ff9f1042c5d4c70c3c9400
Jorge Iza <izajorge7@gmail.com>
2019-10-31 16:38:00 -0500

    v1.0

---
+++ sistemaDistribuido/src/main/java/com/mycompany/principal/main.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.principal;
+
+import com.mycompany.entidades.Producto;
+import com.mycompany.sesion.ProductoJpaController;
+import java.math.BigDecimal;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+/**
+ *
+ * @author Aplicaciones D
+ */
+public class main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("laboratorio");
+        ProductoJpaController pjc = new ProductoJpaController(emf);
+        
+        Producto p = new Producto();//de entidades  ctrl + espacio
+        p.setNombre("mantequilla");
+        p.setPrecio(BigDecimal.valueOf(13.87));
+        
+        pjc.create(p);
+        
+    }
+    
+}
